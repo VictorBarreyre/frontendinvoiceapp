3d52d424440b5a0c19c338cafef705675f052e01
Victor Barreyre <barreyrevictor.contact@gmail.com>
2024-05-20 11:48:43 +0200

    card abo ui

--- roots/Abo.jsx
+++ roots/Abo.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { CardElement, useStripe, useElements, Elements } from '@stripe/react-stripe-js';
 import {
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
     Button,
     Heading,
     Flex,
@@ -18,129 +23,7 @@ import { useNavigate } from 'react-router-dom';
 import { CheckIcon } from '@chakra-ui/icons';
 import { useInvoiceData } from '../src/context/InvoiceDataContext';
 
-const stripePromise = loadStripe('pk_test_51OwLFM00KPylCGutjKAkwhqleWEzuvici1dQUPCIvZHofEzLtGyM9Gdz5zEfvwSZKekKRgA1el5Ypnw7HLfYWOuB00ZdrKdygg');
 
-const appearance = {
-    theme: 'flat',
-    variables: {
-        fontFamily: ' "Gill Sans", sans-serif',
-        fontLineHeight: '1.5',
-        borderRadius: '10px',
-        colorBackground: '#F6F8FA',
-        accessibleColorOnColorPrimary: '#262626'
-    },
-    rules: {
-        '.Block': {
-            backgroundColor: 'var(--colorBackground)',
-            boxShadow: 'none',
-            padding: '12px'
-        },
-        '.Input': {
-            padding: '12px'
-        },
-        '.Input:disabled, .Input--invalid:disabled': {
-            color: 'lightgray'
-        },
-        '.Tab': {
-            padding: '10px 12px 8px 12px',
-            border: 'none'
-        },
-        '.Tab:hover': {
-            border: 'none',
-            boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 7px rgba(18, 42, 66, 0.04)'
-        },
-        '.Tab--selected, .Tab--selected:focus, .Tab--selected:hover': {
-            border: 'none',
-            backgroundColor: '#fff',
-            boxShadow: '0 0 0 1.5px var(--colorPrimaryText), 0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 7px rgba(18, 42, 66, 0.04)'
-        },
-        '.Label': {
-            fontWeight: '500'
-        }
-    }
-};
-
-const SubscribeForm = () => {
-    const stripe = useStripe();
-    const elements = useElements();
-    const [email, setEmail] = useState('');
-    const { invoiceData, baseUrl } = useInvoiceData();
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        if (!stripe || !elements) {
-            return;
-        }
-
-        const cardElement = elements.getElement(CardElement);
-
-        // Créer un payment method
-        const { error, paymentMethod } = await stripe.createPaymentMethod({
-            type: 'card',
-            card: cardElement,
-            billing_details: {
-                email: email,
-            },
-        });
-
-        if (error) {
-            console.error(error);
-            return;
-        }
-
-        // Appeler le backend pour créer un abonnement
-        const response = await fetch(`${baseUrl}/create-subscription`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: email,
-                payment_method: paymentMethod.id,
-            }),
-        });
-
-        const subscription = await response.json();
-
-        if (subscription.error) {
-            console.error(subscription.error.message);
-            return;
-        }
-
-        const { clientSecret } = subscription;
-
-        const result = await stripe.confirmCardPayment(clientSecret);
-
-        if (result.error) {
-            console.error(result.error.message);
-        } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                console.log('Subscription succeeded');
-                // Rediriger vers une page de succès ou afficher un message de succès
-            }
-        }
-    };
-
-    return (
-        <form onSubmit={handleSubmit} style={{ width: '100%', maxWidth: '400px', }}>
-            <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email"
-                required
-                style={{ marginBottom: '20px', padding: '10px', width: '100%', borderRadius: '5px', border: '1px solid #ccc' }}
-            />
-            <div style={{ marginBottom: '20px' }}>
-                <CardElement options={{ appearance }} />
-            </div>
-            <Button mt='2rem' mb='2rem' w={{ base: '100%', lg: 'unset' }} color='white' borderRadius='30px' backgroundColor='black' type="submit" disabled={!stripe}>
-                Souscrire
-            </Button>
-        </form>
-    );
-};
 
 const Abo = () => {
     const [product, setProduct] = useState(null);
@@ -170,14 +53,14 @@ const Abo = () => {
 
     const handleSubscribeClick = () => {
         navigate('/checkout', { state: { priceId: selectedPriceId } });
-      };
+    };
 
     if (loading) {
-        return ( 
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}>
-        <Spinner color='#745FF2' size="md" />
-      </div>)
-      ;
+        return (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}>
+                <Spinner color='#745FF2' size="md" />
+            </div>)
+            ;
     }
 
     if (!product) {
@@ -190,94 +73,123 @@ const Abo = () => {
 
     return (
         <div className='flex-stepper'>
-      <div className="stepper-container">
-        <div className="tabs-container">
-          <Flex direction="column" alignItems="center">
-            <Heading fontSize={{ base: '24px', lg: '26px' }} mb='1rem'>Choississez votre formule d'abonnements</Heading>
-            <Text w='70%' textAlign='center' mb='2rem'>
-              Une fois votre abonnement créé, nous vous enverrons un e-mail contenant un récapulatif de votre formule <br />
-              et un mot de passe provisoire que nous vous invitons à modifier dans votre espace profil.
-            </Text>
-            <Flex wrap="wrap" justify="center" spacing={4}>
-              {monthlyPrice && (
-                <Box className='neue-up'
-                  borderWidth="1px"
-                  backgroundColor='#fdfdfd'
-                  borderColor={selectedPlan === 'yearly' ? 'inherit' : '#745FF2'}
-                  borderRadius="lg"
-                  overflow="hidden"
-                  p={6} mr={4}
-                  maxW="sm"
-                  opacity={selectedPlan === 'yearly' ? 0.6 : 1}
-                  onClick={() => setSelectedPlan('monthly')}
-                  cursor="pointer"
-                  _hover={{ opacity: 1 }}>
+            <div className="stepper-container">
+                <div className="tabs-container">
+                    <Flex direction="column" alignItems="center">
+                        <Heading fontSize={{ base: '24px', lg: '26px' }} mb='1rem'>Choississez votre formule d'abonnements</Heading>
+                        <Text w='70%' textAlign='center' mb='2rem'>
+                            Une fois votre abonnement créé, nous vous enverrons un e-mail contenant un récapulatif de votre formule <br />
+                            et un mot de passe provisoire que nous vous invitons à modifier dans votre espace profil.
+                        </Text>
+                        <Flex direction='column' w='25rem' justify="center" gap='20px'>
+                            <Accordion  allowToggle>
+                                {monthlyPrice && (
+                                    <AccordionItem     
+                                    className='neue-up'
+                                    borderWidth="1px"
+                                    backgroundColor='#fdfdfd'
+                                    borderColor={selectedPlan === 'yearly' ? 'inherit' : '#745FF2'}
+                                    borderRadius="lg"
+                                    overflow="hidden"
+                                    pt='0.5rem' 
+                                    pb='0.5rem' 
+                                    pl='1rem' 
+                                    pr='1rem'
+                                  
+                                    maxW="sm"
+                                    opacity={selectedPlan === 'yearly' ? 0.6 : 1}
+                                    onClick={() => setSelectedPlan('monthly')}
+                                    cursor="pointer">
+                                        <AccordionButton _hover={{ boxShadow: 'none' }} onClick={() => setSelectedPlan('monthly')}>
+                                            <Box flex="1" textAlign="left">
+                                                <Heading color={selectedPlan === 'yearly' ? 'inherit' : '#745FF2'} fontSize="xl" mb={4}>Paiement mensuel</Heading>
+                                                <Heading mt='1rem' size="sm">
+                                                    {monthlyPrice.unit_amount / 100} {monthlyPrice.currency.toUpperCase()} / mois
+                                                </Heading>
+                                            </Box>
+                                            <AccordionIcon />
+                                        </AccordionButton>
+                                        <AccordionPanel pb={4}>
+                                            <List mt='1.5rem' pt='1.5rem' borderTopWidth='1px' spacing={3}>
+                                                <ListItem>
+                                                    <ListIcon as={CheckIcon} color='#745FF2' />
+                                                    {product.description}
+                                                </ListItem>
+                                                <ListItem>
+                                                    <ListIcon as={CheckIcon} color='#745FF2' />
+                                                    Assumenda, quia temporibus eveniet a libero incidunt suscipit
+                                                </ListItem>
+                                                <ListItem>
+                                                    <ListIcon as={CheckIcon} color='#745FF2' />
+                                                    Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
+                                                </ListItem>
+                                            </List>
+                                        </AccordionPanel>
+                                    </AccordionItem>
 
-                  <Heading color={selectedPlan === 'yearly' ? 'inherit' : '#745FF2'} fontSize="xl" mb={4}>Paiement mensuel</Heading>
-                  <Heading mb='1rem' size="sm">{product.description}</Heading>
-                  <Heading mb='1rem' mt='1rem' size="md">
-                    {monthlyPrice.unit_amount / 100} {monthlyPrice.currency.toUpperCase()} / mois
-                  </Heading>
-                  <List mt='1.5rem' pt='1.5rem' borderTopWidth='1px' spacing={3}>
-                    <ListItem>
-                      <ListIcon as={CheckIcon} color='#745FF2' />
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit
-                    </ListItem>
-                    <ListItem>
-                      <ListIcon as={CheckIcon} color='#745FF2' />
-                      Assumenda, quia temporibus eveniet a libero incidunt suscipit
-                    </ListItem>
-                    <ListItem>
-                      <ListIcon as={CheckIcon} color='#745FF2' />
-                      Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
-                    </ListItem>
-                  </List>
-                </Box>
-              )}
-              {yearlyPrice && (
-                <Box className='neue-up'
-                  borderWidth="1px"
-                  backgroundColor='#fdfdfd'
-                  borderColor={selectedPlan === 'monthly' ? 'inherit' : '#745FF2'}
-                  borderRadius="lg"
-                  overflow="hidden"
-                  p={6}
-                  ml={4}
-                  maxW="sm"
-                  onClick={() => setSelectedPlan('yearly')}
-                  opacity={selectedPlan === 'monthly' ? 0.6 : 1}
-                  cursor="pointer"
-                  _hover={{ opacity: 1 }}>
+                                )}
+                            </Accordion>
 
-                  <Heading color={selectedPlan === 'monthly' ? 'inherit' : '#745FF2'} fontSize="xl" mb={4}>Paiement annuel</Heading>
-                  <Heading mb='1rem' size="sm">{product.description}</Heading>
-                  <Heading mb='1rem' mt='1rem' size="md">
-                    {yearlyPrice.unit_amount / 100} {yearlyPrice.currency.toUpperCase()} / an
-                  </Heading>
-                  <List mt='1.5rem' pt='1.5rem' borderTopWidth='1px' spacing={3}>
-                    <ListItem>
-                      <ListIcon as={CheckIcon} color='#745FF2' />
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit
-                    </ListItem>
-                    <ListItem>
-                      <ListIcon as={CheckIcon} color='#745FF2' />
-                      Assumenda, quia temporibus eveniet a libero incidunt suscipit
-                    </ListItem>
-                    <ListItem>
-                      <ListIcon as={CheckIcon} color='#745FF2' />
-                      Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
-                    </ListItem>
-                  </List>
-                </Box>
-              )}
-            </Flex>
-            <Button mt='2rem' mb='2rem' w={{ base: '100%', lg: 'unset' }} color='white' borderRadius='30px' backgroundColor='black' onClick={handleSubscribeClick}>
-              Souscrire
-            </Button>
-          </Flex>
+
+                            <Accordion allowToggle>
+                            {yearlyPrice && (
+                                    <AccordionItem 
+                                    w='100%'   
+                                    className='neue-up'
+                                    borderWidth="1px"
+                                    backgroundColor='#fdfdfd'
+                                    borderColor={selectedPlan === 'monthly' ? 'inherit' : '#745FF2'}
+                                    borderRadius="lg"
+                                    overflow="hidden"
+                                    pt='0.5rem' 
+                                    pb='0.5rem' 
+                                    pl='1rem' 
+                                    pr='1rem'
+                                 
+                                    maxW="sm"
+                                    opacity={selectedPlan === 'monthly' ? 0.6 : 1}
+                                    onClick={() => setSelectedPlan('yearly')}
+                                    cursor="pointer">
+                                        <AccordionButton _hover={{ boxShadow: 'none' }} onClick={() => setSelectedPlan('yearly')}>
+                                            <Box flex="1" textAlign="left">
+                                            <Heading color={selectedPlan === 'monthly' ? 'inherit' : '#745FF2'} fontSize="xl" mb={4}>Paiement annuel</Heading>
+                                            <Heading mt='1rem' size="sm">
+                                                {yearlyPrice.unit_amount / 100} {yearlyPrice.currency.toUpperCase()} / an
+                                                </Heading>
+                                            </Box>
+                                            <AccordionIcon />
+                                        </AccordionButton>
+                                        <AccordionPanel pb={4}>
+                                            <List mt='1.5rem' pt='1.5rem' borderTopWidth='1px' spacing={3}>
+                                                <ListItem>
+                                                    <ListIcon as={CheckIcon} color='#745FF2' />
+                                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit
+                                                </ListItem>
+                                                <ListItem>
+                                                    <ListIcon as={CheckIcon} color='#745FF2' />
+                                                    Assumenda, quia temporibus eveniet a libero incidunt suscipit
+                                                </ListItem>
+                                                <ListItem>
+                                                    <ListIcon as={CheckIcon} color='#745FF2' />
+                                                    Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
+                                                </ListItem>
+                                            </List>
+                                        </AccordionPanel>
+                                    </AccordionItem>
+
+                                )}
+                            </Accordion>
+
+
+        
+                        </Flex>
+                        <Button mt='2rem' mb='2rem' w={{ base: '100%', lg: 'unset' }} color='white' borderRadius='30px' backgroundColor='black' onClick={handleSubscribeClick}>
+                            Souscrire
+                        </Button>
+                    </Flex>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
     );
 };
 
