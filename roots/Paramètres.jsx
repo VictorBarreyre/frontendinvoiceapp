8689ec6ad9588c3006d489bee6e6f2c2f8986dd7
Victor Barreyre <barreyrevictor.contact@gmail.com>
2024-06-18 09:50:18 +0200

    reset mdp ok

--- roots/Paramètres.jsx
+++ roots/Paramètres.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useAuth } from '../src/context/AuthContext';
-import { useInvoiceData } from '../src/context/InvoiceDataContext';
 import {
   Button,
   Box,
@@ -26,7 +25,7 @@ import {
   VStack,
 } from '@chakra-ui/react';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const Paramètres = () => {
@@ -46,6 +45,7 @@ const Paramètres = () => {
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isPasswordVerified, setIsPasswordVerified] = useState(false);
+  const [showPasswordInput, setShowPasswordInput] = useState(false);
 
   const handlePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -178,153 +178,26 @@ const Paramètres = () => {
       <div className="stepper-container">
         <div className="tabs-container">
           <Flex direction='column' h={{ base: '100vh', lg: 'content' }} >
-            <Heading pb='1rem' mb={{ base: '0rem', lg: '2rem' }} borderBottom={{ base: 'unset', lg: '2px solid #efefef' }} fontSize={{ base: '22px', lg: '26px' }}>Paramètres de votre compte</Heading>
-            <Flex direction='column' h={{ base: 'content', lg: 'content' }}  mb='1rem'>
-            {!isPasswordVerified ? (
-              <Flex direction='column' mt="4" w={{ base: '100%', lg: '25rem' }} borderBottom='1px solid #efefef' pb='1rem'>
-                <FormControl  isRequired>
-                <Heading mb='1rem' size="sm">Réinitialiser votre mot de passe</Heading>
-                  <InputGroup
-                   width={{ base: 'unset', lg: '25rem' }}
-                    mb='0.5rem'>
-                    <ChakraInput
-                      className='neue-down'
-                      _focus={{ borderColor: "#745FF2", boxShadow: "none" }}
-                      id="password"
-                      type={showPassword ? "text" : "password"}
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      placeholder='Entrez votre mot de passe actuel'
-                    />
-                    <InputRightElement width="4.5rem">
-                      <IconButton
-                        background='none'
-                        h="2rem"
-                        size="lg"
-                        onClick={handlePasswordVisibility}
-                        icon={showPassword ? <ViewOffIcon /> : <ViewIcon />}
-                        sx={{
-                          _hover: { background: 'none', boxShadow: 'none', transform: 'none' },
-                          _active: { background: 'none', boxShadow: 'none', transform: 'none' },
-                          _focus: { boxShadow: 'none' }
-                        }}
-                      />
-                    </InputRightElement>
-                  </InputGroup>
-                  {passwordError && (
-                    <Text color="red.500" mt="2">
-                      {passwordError}
-                    </Text>
-                  )}
-                </FormControl>
-                <Chakralink mt='1rem' color="#745FF2" onClick={handleVerifyPassword} >
-                  Vérifier le mot de passe
-                </Chakralink>
-              </Flex>
-            ) : (
-              <Flex direction='column' mt="4" w={{ base: '100%', lg: 'content' }} borderBottom='1px solid #efefef' pb='1rem'>
-                <form onSubmit={handleSubmit}>
-                  <VStack spacing={4}>
+            <Heading pb='1rem' mb={{ base: '0rem', lg: '2rem' }} borderBottom={{ base: 'unset', lg: '2px solid #efefef' }} fontSize={{ base: '22px', lg: '26px' }}>
+              Paramètres de votre compte
+            </Heading>
+            <Flex direction='column' h={{ base: 'content', lg: 'content' }} mb='1rem'>
+              <Heading size="sm" mb="0">{showPasswordInput ? "Vérifier votre mot de passe" : "Votre mot de passe"}</Heading>
+              {!isPasswordVerified ? (
+                <Flex direction='column' w={{ base: '100%', lg: '25rem' }} borderBottom='1px solid #efefef' pb='1rem'>
+                  {showPasswordInput && (
                     <FormControl isRequired>
-                      <FormLabel htmlFor="newPassword">Nouveau mot de passe</FormLabel>
-                      <InputGroup >
-                        <ChakraInput
-                          w={{ base: '100%', lg: '25rem' }}
-                          className='neue-down'
-                          _focus={{ borderColor: "#745FF2", boxShadow: "none" }}
-                          id="newPassword"
-                          type={showNewPassword ? "text" : "password"}
-                          value={newPassword}
-                          onChange={(e) => setNewPassword(e.target.value)}
-                        />
-                        <InputRightElement width="4.5rem">
-                          <IconButton
-                            background='none'
-                            h="2rem"
-                            size="lg"
-                            onClick={handleNewPasswordVisibility}
-                            icon={showNewPassword ? <ViewOffIcon /> : <ViewIcon />}
-                            sx={{
-                              _hover: { background: 'none', boxShadow: 'none', transform: 'none' },
-                              _active: { background: 'none', boxShadow: 'none', transform: 'none' },
-                              _focus: { boxShadow: 'none' }
-                            }}
-                          />
-                        </InputRightElement>
-                      </InputGroup>
-                    </FormControl>
-                    <FormControl isRequired>
-                      <FormLabel htmlFor="confirmPassword">Confirmez le mot de passe</FormLabel>
-                      <InputGroup>
+                      <InputGroup
+                        width={{ base: 'unset', lg: '25rem' }}
+                        mt='1rem'>
                         <ChakraInput
                           className='neue-down'
                           _focus={{ borderColor: "#745FF2", boxShadow: "none" }}
-                          id="confirmPassword"
-                          type={showConfirmPassword ? "text" : "password"}
-                          value={confirmPassword}
-                          onChange={(e) => setConfirmPassword(e.target.value)}
-                        />
-                        <InputRightElement width="4.5rem">
-                          <IconButton
-                            background='none'
-                            h="2rem"
-                            size="lg"
-                            onClick={handleConfirmPasswordVisibility}
-                            icon={showConfirmPassword ? <ViewOffIcon /> : <ViewIcon />}
-                            sx={{
-                              _hover: { background: 'none', boxShadow: 'none', transform: 'none' },
-                              _active: { background: 'none', boxShadow: 'none', transform: 'none' },
-                              _focus: { boxShadow: 'none' }
-                            }}
-                          />
-                        </InputRightElement>
-                      </InputGroup>
-                    </FormControl>
-                    {errorMessage && <ChakraText mt='1rem' textAlign='center' color="red">{errorMessage}</ChakraText>}
-                    <Button type="submit" color='white' borderRadius='30px' backgroundColor='black' mt="4" colorScheme="gray" isLoading={isSubmitting}>
-                      Réinitialiser le mot de passe
-                    </Button>
-                  </VStack>
-                </form>
-              </Flex>
-            )}
-
-       
-          <Flex mt='2rem' w={{ base: '100%', lg: '25rem' }} borderBottom='1px solid #efefef' pb='1rem' direction='column'> 
-          <Heading mb='0.5rem' size="sm">Vos données personnelles</Heading>
-             <Chakralink mt="0.5rem" color="#745FF2"  onClick={handleDownloadData}  colorScheme="blue">
-              Télécharger mes données
-            </Chakralink>
-            </Flex>
-
-            <Flex mt='2rem' w={{ base: '100%', lg: '25rem' }} direction='column'> 
-            <Heading mb='0.5rem' size="sm"> Suppression du compte</Heading>
-            <Chakralink textAlign={{ base: 'left', lg: 'unset' }} onClick={onOpen} color='red !important' mt="0.5rem">
-              Supprimer mon compte
-            </Chakralink>
-            </Flex>
-
-            <AlertDialog
-              isOpen={isOpen}
-              leastDestructiveRef={cancelRef}
-              onClose={onClose}
-            >
-              <AlertDialogOverlay>
-                <AlertDialogContent>
-                  <AlertDialogHeader fontSize="lg" fontWeight="bold">
-                    Supprimer mon compte
-                  </AlertDialogHeader>
-
-                  <AlertDialogBody>
-                    Êtes-vous sûr de vouloir supprimer votre compte ? Cette action est irréversible.
-                    <FormControl mt={4}>
-                      <FormLabel>Mot de passe</FormLabel>
-                      <InputGroup>
-                        <ChakraInput
-                          className='neue-down'
+                          id="password"
                           type={showPassword ? "text" : "password"}
                           value={password}
                           onChange={(e) => setPassword(e.target.value)}
+                          placeholder='Entrez votre mot de passe actuel'
                         />
                         <InputRightElement width="4.5rem">
                           <IconButton
@@ -336,7 +209,7 @@ const Paramètres = () => {
                             sx={{
                               _hover: { background: 'none', boxShadow: 'none', transform: 'none' },
                               _active: { background: 'none', boxShadow: 'none', transform: 'none' },
-                              _focus: { color: '#745FF2', boxShadow: 'none' }
+                              _focus: { boxShadow: 'none' }
                             }}
                           />
                         </InputRightElement>
@@ -347,19 +220,167 @@ const Paramètres = () => {
                         </Text>
                       )}
                     </FormControl>
-                  </AlertDialogBody>
+                  )}
+                  <Chakralink mt="1rem" color="#745FF2" onClick={() => {
+                    if (showPasswordInput) {
+                      handleVerifyPassword();
+                    } else {
+                      setShowPasswordInput(true);
+                    }
+                  }}>
+                    {showPasswordInput ? "Vérifier votre mot de passe" : "Changer votre mot de passe"}
+                  </Chakralink>
+                </Flex>
+              ) : (
+                <Flex direction='column' mt='1rem' w={{ base: '100%', lg: 'content' }} borderBottom='1px solid #efefef' pb='1rem'>
+                  <form onSubmit={handleSubmit}>
+                    <VStack align="start" spacing={4}>
+                      <FormControl isRequired>
+                        <FormLabel htmlFor="newPassword">Nouveau mot de passe</FormLabel>
+                        <InputGroup w={{ base: '100%', lg: '25rem' }}>
+                          <ChakraInput
+                            className='neue-down'
+                            _focus={{ borderColor: "#745FF2", boxShadow: "none" }}
+                            id="newPassword"
+                            type={showNewPassword ? "text" : "password"}
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                          />
+                          <InputRightElement width="4.5rem">
+                            <IconButton
+                              background='none'
+                              h="2rem"
+                              size="lg"
+                              onClick={handleNewPasswordVisibility}
+                              icon={showNewPassword ? <ViewOffIcon /> : <ViewIcon />}
+                              sx={{
+                                _hover: { background: 'none', boxShadow: 'none', transform: 'none' },
+                                _active: { background: 'none', boxShadow: 'none', transform: 'none' },
+                                _focus: { boxShadow: 'none' }
+                              }}
+                            />
+                          </InputRightElement>
+                        </InputGroup>
+                      </FormControl>
+                      <FormControl isRequired>
+                        <FormLabel htmlFor="confirmPassword">Confirmez le mot de passe</FormLabel>
+                        <InputGroup w={{ base: '100%', lg: '25rem' }}>
+                          <ChakraInput
+                            className='neue-down'
+                            _focus={{ borderColor: "#745FF2", boxShadow: "none" }}
+                            id="confirmPassword"
+                            type={showConfirmPassword ? "text" : "password"}
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                          />
+                          <InputRightElement width="4.5rem">
+                            <IconButton
+                              background='none'
+                              h="2rem"
+                              size="lg"
+                              onClick={handleConfirmPasswordVisibility}
+                              icon={showConfirmPassword ? <ViewOffIcon /> : <ViewIcon />}
+                              sx={{
+                                _hover: { background: 'none', boxShadow: 'none', transform: 'none' },
+                                _active: { background: 'none', boxShadow: 'none', transform: 'none' },
+                                _focus: { boxShadow: 'none' }
+                              }}
+                            />
+                          </InputRightElement>
+                        </InputGroup>
+                      </FormControl>
+                    
+                      {errorMessage && <ChakraText mt='1rem' textAlign='center' color="red">{errorMessage}</ChakraText>}
+                      <Button variant="link"
+                        type="submit"
+                        borderRadius='30px'
+                        backgroundColor='transparent'
+                        mt="4"
+                        color="#745FF2"
+                        isLoading={isSubmitting}
+                        fontWeight='400'
+                        sx={{
+                          _hover: { background: 'none', boxShadow: 'none', textDecoration: 'underline' },
+                        }}>
+                        Réinitialiser le mot de passe
+                      </Button>
+        
+                    </VStack>
+                  </form>
+                </Flex>
+              )}
 
-                  <AlertDialogFooter>
-                    <Button borderRadius='30px' ref={cancelRef} onClick={onClose}>
-                      Annuler
-                    </Button>
-                    <Button borderRadius='30px' colorScheme="red" onClick={handleDeleteClick} ml={3}>
-                      Supprimer
-                    </Button>
-                  </AlertDialogFooter>
-                </AlertDialogContent>
-              </AlertDialogOverlay>
-            </AlertDialog>
+
+              <Flex mt='2rem' w={{ base: '100%', lg: '25rem' }} borderBottom='1px solid #efefef' pb='1rem' direction='column'>
+                <Heading mb='0.5rem' size="sm">Vos données personnelles</Heading>
+                <Chakralink mt="0.5rem" color="#745FF2" onClick={handleDownloadData} colorScheme="blue">
+                  Télécharger mes données
+                </Chakralink>
+              </Flex>
+
+              <Flex mt='2rem' w={{ base: '100%', lg: '25rem' }} direction='column'>
+                <Heading mb='0.5rem' size="sm"> Suppression du compte</Heading>
+                <Chakralink textAlign={{ base: 'left', lg: 'unset' }} onClick={onOpen} color='red !important' mt="0.5rem">
+                  Supprimer mon compte
+                </Chakralink>
+              </Flex>
+
+              <AlertDialog
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+              >
+                <AlertDialogOverlay>
+                  <AlertDialogContent>
+                    <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                      Supprimer mon compte
+                    </AlertDialogHeader>
+
+                    <AlertDialogBody>
+                      Êtes-vous sûr de vouloir supprimer votre compte ? Cette action est irréversible.
+                      <FormControl mt={4}>
+                        <FormLabel>Mot de passe</FormLabel>
+                        <InputGroup>
+                          <ChakraInput
+                            className='neue-down'
+                            type={showPassword ? "text" : "password"}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                          <InputRightElement width="4.5rem">
+                            <IconButton
+                              background='none'
+                              h="2rem"
+                              size="lg"
+                              onClick={handlePasswordVisibility}
+                              icon={showPassword ? <ViewOffIcon /> : <ViewIcon />}
+                              sx={{
+                                _hover: { background: 'none', boxShadow: 'none', transform: 'none' },
+                                _active: { background: 'none', boxShadow: 'none', transform: 'none' },
+                                _focus: { color: '#745FF2', boxShadow: 'none' }
+                              }}
+                            />
+                          </InputRightElement>
+                        </InputGroup>
+                        {passwordError && (
+                          <Text color="red.500" mt="2">
+                            {passwordError}
+                          </Text>
+                        )}
+                      </FormControl>
+                    </AlertDialogBody>
+
+                    <AlertDialogFooter>
+                      <Button borderRadius='30px' ref={cancelRef} onClick={onClose}>
+                        Annuler
+                      </Button>
+                      <Button borderRadius='30px' colorScheme="red" onClick={handleDeleteClick} ml={3}>
+                        Supprimer
+                      </Button>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialogOverlay>
+              </AlertDialog>
             </Flex>
           </Flex>
         </div>
