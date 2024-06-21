c87b24e3574d8acdcfddb628ba937c872cef4955
Vr <vr@MacBook-Pro-de-Vr.local>
2024-06-21 08:03:52 +0200

    facture mobile

--- roots/Factures.jsx
+++ roots/Factures.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth } from '../src/context/AuthContext';
-import { 
+import {
   Button, Link as Chakralink, Box, Heading, Text, Flex, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Image, useDisclosure,
-  Table, Thead, Tbody, Tr, Th, Td, TableContainer
+  Table, Thead, Tbody, Tr, Th, Td, TableContainer, useMediaQuery
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
@@ -12,6 +12,7 @@ const Factures = () => {
   const [message, setMessage] = useState('');
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [selectedInvoiceImage, setSelectedInvoiceImage] = useState('');
+  const [isMobile] = useMediaQuery("(max-width: 768px)"); // Détecter les appareils mobiles
 
   useEffect(() => {
     const loadInvoices = async () => {
@@ -38,30 +39,71 @@ const Factures = () => {
           <Flex direction='column' h={{ base: '100vh', lg: 'content' }}>
             <Heading pb='1rem' mb={{ base: '0rem', lg: '2rem' }} borderBottom={{ base: 'unset', lg: '2px solid #efefef' }} fontSize={{ base: '22px', lg: '26px' }}>Vos Factures</Heading>
             {message && <Text>{message}</Text>}
-            <TableContainer>
-              <Table variant='simple'>
-                <Thead>
-                  <Tr>
-                    <Th>Numéro de Facture</Th>
-                    <Th>Montant</Th>
-                    <Th textAlign='end'>Action</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  {invoices.map(invoice => (
-                    <Tr key={invoice._id}>
-                      <Td>Facture n°{invoice.number}</Td>
-                      <Td>{invoice.montant}{invoice.devise}</Td>
-                      <Td textAlign='end'>
-                        <Chakralink color="#745FF2" onClick={() => handlePreviewClick(invoice.urlImage)}>
-                          Voir la facture
-                        </Chakralink>
-                      </Td>
+
+            {isMobile ? (
+              <>
+                <Flex justifyContent="space-between" mt="1rem">
+                  <Text fontFamily="heading"
+                    fontWeight="bold"
+                    textTransform="uppercase"
+                    letterSpacing="wider"
+                    textAlign="center"
+                    mb={2}
+                    pb={3} // padding-bottom: var(--chakra-space-3);
+                    lineHeight="4"
+                    fontSize="xs"
+                    color="gray.600">Facture n°</Text>
+                  <Text fontFamily="heading"
+                    fontWeight="bold"
+                    textTransform="uppercase"
+                    letterSpacing="wider"
+                    textAlign="center"
+                    mb={2}
+                    pb={3} // padding-bottom: var(--chakra-space-3);
+                    lineHeight="4"
+                    fontSize="xs"
+                    color="gray.600">Montant</Text>
+                </Flex>
+                {invoices.map(invoice => (
+                  <Box key={invoice._id} borderBottom="1px solid #efefef" pt='1rem' pb='1rem' mb='1rem'>
+                    <Flex justifyContent="space-between">
+                      <Text>Facture n°{invoice.number}</Text>
+                      <Text>{invoice.montant}{invoice.devise}</Text>
+                    </Flex>
+                    <Flex justifyContent="space-between">
+                      <Chakralink color="#745FF2" onClick={() => handlePreviewClick(invoice.urlImage)}>
+                        Voir la facture
+                      </Chakralink>
+                    </Flex>
+                  </Box>
+                ))}
+              </>
+            ) : (
+              <TableContainer>
+                <Table variant='simple'>
+                  <Thead>
+                    <Tr>
+                      <Th>Numéro de Facture</Th>
+                      <Th>Montant</Th>
+                      <Th textAlign='end'>Action</Th>
                     </Tr>
-                  ))}
-                </Tbody>
-              </Table>
-            </TableContainer>
+                  </Thead>
+                  <Tbody>
+                    {invoices.map(invoice => (
+                      <Tr key={invoice._id}>
+                        <Td>Facture n°{invoice.number}</Td>
+                        <Td>{invoice.montant}{invoice.devise}</Td>
+                        <Td textAlign='end'>
+                          <Chakralink color="#745FF2" onClick={() => handlePreviewClick(invoice.urlImage)}>
+                            Voir la facture
+                          </Chakralink>
+                        </Td>
+                      </Tr>
+                    ))}
+                  </Tbody>
+                </Table>
+              </TableContainer>
+            )}
           </Flex>
         </div>
       </div>
